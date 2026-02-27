cf5fc33d1367b3fd203a20872017d9160a917537
Felix Kebaki <felixkebaki@gmail.com>
2026-02-27 09:06:25 +0300

    Send email 2

--- index.js
+++ index.js
@@ -3,115 +3,117 @@
 
 
 
-// let First=document.getElementById("FirstName");
-// let Last=document.getElementById("LastName");
-// let Email=document.getElementById("Email");
-// let Message=document.getElementById("Message");
-// let LikeBefore=document.querySelector(".LikeBefore");
+let First=document.getElementById("FirstName");
+let Last=document.getElementById("LastName");
+let Email=document.getElementById("Email");
+let Message=document.getElementById("Message");
+let LikeBefore=document.querySelector(".LikeBefore");
 
-// function sendMail(){
-//     if(First.value!="" && Last.value!="" && Email.value!="" && Message.value!=""){
-//         (function(){
-//             emailjs.init("JkeST0HAGsnbuzM5u");
-//         })();
+
+emailjs.init({
+    publicKey: "JkeST0HAGsnbuzM5u",
+});
+
+function sendMail(){
+    if(First.value!="" && Last.value!="" && Email.value!="" && Message.value!=""){
     
-//         var params = {
-//             FirstName :  First.value,
-//             LastName : Last.value,
-//             Email : Email.value,
-//             Message : Message.value,
-//         }
+        var params = {
+            FirstName :  First.value,
+            LastName : Last.value,
+            Email : Email.value,
+            Message : Message.value,
+        }
     
-//         var serviceId="service_vfzczjf";
-//         var templateId="template_gesy2yu";
+        var serviceId="service_vfzczjf";
+        var templateId="template_gesy2yu";
     
-//         emailjs.send(serviceId,templateId,params)
-//         .then(res => {
-//                 LikeBefore.style.zIndex="1";
+        emailjs.send(serviceId,templateId,params)
+        .then(res => {
+                LikeBefore.style.zIndex="1";
 
-//                 if(LikeBefore.style.zIndex=="1"){
-//                     setTimeout(()=>{
-//                         LikeBefore.style.zIndex="-9999";
-//                     },3000);
-//                 }
-//             }
+                if(LikeBefore.style.zIndex=="1"){
+                    setTimeout(()=>{
+                        LikeBefore.style.zIndex="-9999";
+                    },3000);
+                }
+            }
 
-//         )
-//         .catch();
+        )
+        .catch();
     
-//         params={
-//             FirstName : document.getElementById("FirstName").value="",
-//             LastName : document.getElementById("LastName").value="",
-//             Email : document.getElementById("Email").value="",
-//             Message : document.getElementById("Message").value="",
-//         }
-//     }
-//     else{
-//         window.alert("Please put all your details first");
-//     }
-// }
-
-
-
-
-
-// Get elements
-let First = document.getElementById("FirstName");
-let Last = document.getElementById("LastName");
-let Email = document.getElementById("Email");
-let Message = document.getElementById("Message");
-let LikeBefore = document.querySelector(".LikeBefore");
-
-// ✅ Initialize EmailJS (v4 style)
-window.onload = function () {
-    emailjs.init({
-        publicKey: "fWj6sGTmzVaMoqup2",
-    });
-};
-
-function sendMail() {
-
-    // Validate inputs
-    if (First.value && Last.value && Email.value && Message.value) {
-
-        let params = {
-            FirstName: First.value,
-            LastName: Last.value,
-            Email: Email.value,
-            Message: Message.value,
-        };
-
-        // Send email
-        emailjs.send("service_70rsc46", "template_vr84iz8", params)
-            .then(function (response) {
-
-                console.log("SUCCESS!", response);
-
-                // Show success message
-                LikeBefore.style.zIndex = "1";
-
-                setTimeout(() => {
-                    LikeBefore.style.zIndex = "-9999";
-                }, 3000);
-
-                // Clear inputs
-                First.value = "";
-                Last.value = "";
-                Email.value = "";
-                Message.value = "";
-
-            })
-            .catch(function (error) {
-
-                console.log("FAILED...", error);
-                alert("Failed to send message. Check console.");
-
-            });
-
-    } else {
-        alert("Please put all your details first");
+        params={
+            FirstName : document.getElementById("FirstName").value="",
+            LastName : document.getElementById("LastName").value="",
+            Email : document.getElementById("Email").value="",
+            Message : document.getElementById("Message").value="",
+        }
+    }
+    else{
+        window.alert("Please put all your details first");
     }
 }
+
+
+
+
+
+// // Get elements
+// let First = document.getElementById("FirstName");
+// let Last = document.getElementById("LastName");
+// let Email = document.getElementById("Email");
+// let Message = document.getElementById("Message");
+// let LikeBefore = document.querySelector(".LikeBefore");
+
+// // ✅ Initialize EmailJS (v4 style)
+// window.onload = function () {
+//     emailjs.init({
+//         publicKey: "fWj6sGTmzVaMoqup2",
+//     });
+// };
+
+// function sendMail() {
+
+//     // Validate inputs
+//     if (First.value && Last.value && Email.value && Message.value) {
+
+//         let params = {
+//             FirstName: First.value,
+//             LastName: Last.value,
+//             Email: Email.value,
+//             Message: Message.value,
+//         };
+
+//         // Send email
+//         emailjs.send("service_70rsc46", "template_vr84iz8", params)
+//             .then(function (response) {
+
+//                 console.log("SUCCESS!", response);
+
+//                 // Show success message
+//                 LikeBefore.style.zIndex = "1";
+
+//                 setTimeout(() => {
+//                     LikeBefore.style.zIndex = "-9999";
+//                 }, 3000);
+
+//                 // Clear inputs
+//                 First.value = "";
+//                 Last.value = "";
+//                 Email.value = "";
+//                 Message.value = "";
+
+//             })
+//             .catch(function (error) {
+
+//                 console.log("FAILED...", error);
+//                 alert("Failed to send message. Check console.");
+
+//             });
+
+//     } else {
+//         alert("Please put all your details first");
+//     }
+// }
 
 
 
